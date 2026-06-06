ae0b1239ab0cab1ba2cd37c0e68326f3a71b2375
mark95477 <mark95477@yandex.ru>
2026-06-06 18:27:23 +0300

    final code

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	l := log.New(os.Stdout, "INFO:", log.LstdFlags)
+	s := server.NewServer(l)
+	err := s.ListenAndServe()
+	if err != nil {
+		l.Fatal(err)
+	}
 }
+
+// сборка для 6 спринта
